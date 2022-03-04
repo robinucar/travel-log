46bce0375be954e5ef6208e6c291af5590879ee1
Cristian <cristian.christensen@gmail.com>
2022-03-04 22:03:36 +0100

    Frontend: finished??

--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -1,6 +1,8 @@
+import { Link } from "react-router-dom";
 import "./topbar.css"
 
 export default function  TopBar() {
+  const user = true;
   return (
     <div className="top"> 
       <div className="topLeft">
@@ -11,18 +13,32 @@ export default function  TopBar() {
       </div>
       <div className="topCenter">
         <ul className="topList">
-          <li className="topListItem">HOME</li>
-          <li className="topListItem">ABOUT</li>
-          <li className="topListItem">CONTACT</li>
-          <li className="topListItem">WRITE</li>
-          <li className="topListItem">LOGOUT</li>
+          <li className="topListItem"><Link className="link" to="/">HOME</Link></li>
+          <li className="topListItem"><Link className="link" to="/about">ABOUT</Link></li>
+          <li className="topListItem"><Link className="link" to="/contact">CONTACT</Link></li>
+          <li className="topListItem"><Link className="link" to="/write">WRITE</Link></li>
+          <li className="topListItem">
+            {user && "LOGOUT"}
+          </li>
         </ul>
       </div>
       <div className="topRight">
-        <img 
-          className="topImg"
-          src="https://upload.wikimedia.org/wikipedia/commons/9/9a/Gull_portrait_ca_usa.jpg" 
-          alt="" />
+        {
+          user ? (
+            <img 
+            className="topImg"
+            src="https://upload.wikimedia.org/wikipedia/commons/9/9a/Gull_portrait_ca_usa.jpg" 
+            alt="" />
+          ) : (
+            <ul className="topList">
+              <li className="topListItem">
+                <Link className="link" to="/login">LOGIN</Link>
+                <Link className="link" to="/register">REGISTER</Link>
+              </li>
+            </ul>
+          )
+        }
+
         <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
       </div>
     </div>
