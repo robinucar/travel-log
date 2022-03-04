9bd8485b0ab52a06f6cda45236367e0c437748bd
Cristian <cristian.christensen@gmail.com>
2022-03-04 17:14:42 +0100

    Frontend: Working on Single Post

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Home from './pages/home/Home'
-import TopBar from "./topbar/TopBar.jsx"
+import TopBar from "./components/topbar/TopBar.jsx"
+import Single from './pages/single/Single';
+
 
 function App() {
   return (
     <>
     <TopBar/>
-    <Home />
+    <Single />
     </>
   );
 }
