d57c5bad22b2c0a6e798a5c390224af3745652ff
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-03-05 14:34:08 +0000

    Post model and schema

---
+++ api/models/Post.js
@@ -0,0 +1,34 @@
+// require mongoose
+const mongoose = require("mongoose");
+
+// user schema
+
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    photo: {
+      type: String,
+      required: false,
+    },
+
+    username: {
+      type: String,
+      required: true,
+    },
+    categories: {
+      type: Array,
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Post", postSchema);
