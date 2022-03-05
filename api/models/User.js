ff31c30b92d90863aa966c184036573132b42cc0
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-03-05 14:24:59 +0000

    User model and user schema

---
+++ api/models/User.js
@@ -0,0 +1,38 @@
+// require mongoose
+const mongoose = require("mongoose");
+
+// user schema
+
+const userSchema = new mongoose.Schema(
+  {
+      // first name and last name
+    username: {
+      type: String,
+      required: true,
+      uniq: true,
+      // min and max length
+    },
+    email: {
+      type: String,
+      required: true,
+      uniq: true,
+      //valid email format
+    },
+
+    password: {
+      type: String,
+      required: true,
+      //min length
+    },
+
+    // password confirmation
+
+    profilePic: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
