df14ad8fdaef248d690715bbed7457e4b7cee583
samir <s-malik@live.co.uk>
2022-03-05 16:55:27 +0000

    Bcrypt added & working

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,25 +1,29 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require('bcryptjs');
 
 //REGISTER
 router.post("/register", async (req, res) => {
+    
     const {username, email, password} = req.body
+
     try {
+
+        const salt = await bcrypt.genSalt();
+        const hashedPass = await bcrypt.hash(password, salt)
+
         const newUser = new User({
             username,
             email,
-            password
+            password: hashedPass,
         });
 
-        const user = await newUser.save()
-
+        const user = await newUser.save();
         res.status(200).json(user);
-    } catch (err) {
+    }   catch (err) {
         res.status(500).json(err);
     }
 });
-
-
 
 //LOGIN
 
