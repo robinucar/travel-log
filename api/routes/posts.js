759a76f84dcf2d0aff13a4d97ebfa41e9d9b7e8b
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-03-05 23:15:41 +0000

    update post

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -5,12 +5,39 @@ const Post = require("../models/Post");
 //
 // CREATE POST
 router.post("/", async (req, res) => {
-  const newPost = new Post(req.body)
+  const newPost = new Post(req.body);
   try {
-      const savedPost = await newPost.save();
-      res.status(200).json(savedPost)
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
   } catch (err) {
-      res.status(500).json(err)
+    res.status(500).json(err);
+  }
+});
+
+// UPDATE POST
+
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const post = await Post.findById(id);
+    if (post.username === req.body.username) {
+      try {
+        const updatedPost = await Post.findByIdAndUpdate(
+          id,
+          {
+            $set: req.body,
+          },
+          { new: true }
+        );
+        res.json(updatedPost);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(401).json("You can update only your posts...");
+    }
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
