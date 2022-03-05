ca970d2aa9a3ae30bdfb29b6f397e6bb5392a971
samir <s-malik@live.co.uk>
2022-03-05 18:44:22 +0000

    Update Users/ id pathway

---
+++ api/routes/users.js
@@ -0,0 +1,30 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require('bcryptjs');
+const { update } = require("../models/User");
+
+// UPDATE
+router.put("/:id", async (req, res) => {
+    if(req.body.userId === req.params.id){
+        if(req.body.password) {
+            const salt = await bcrypt.genSalt();
+            req.body.passowrd = await bcrypt.hash(req.body.password, salt);
+        }
+
+        try {
+            const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+                $set: req.body,
+            },{
+                new: true, 
+            });
+            res.status(200).json(updatedUser)
+
+        } catch (err) {
+            res.status(500).json(err);
+        }
+
+    }   else{
+            res.status(401).json("You can only updaet your account.");
+        }
+    }
+});
