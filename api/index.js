a499cf4aeab142e888d35bcdd03f4d313cc049e4
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-03-05 20:48:59 +0000

    update users completed

--- api/index.js
+++ api/index.js
@@ -3,21 +3,22 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
-const PORT = 5001;
-
+const userRoute = require("./routes/users");
+const PORT = 5000;
 
 dotenv.config();
 app.use(express.json());
 
 mongoose
-    .connect(process.env.API_URL, {
-     useNewUrlParser: true,
-     useUnifiedTopology: true,
-    })
-    .then(console.log("Connected to MongoDB"))
-    .catch((err) => console.log(err));
+  .connect(process.env.API_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 app.listen(PORT, () => {
   console.log(`Backend is running at ${PORT}`);
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,30 +1,32 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const bcrypt = require('bcryptjs');
-const { update } = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 // UPDATE
 router.put("/:id", async (req, res) => {
-    if(req.body.userId === req.params.id){
-        if(req.body.password) {
-            const salt = await bcrypt.genSalt();
-            req.body.passowrd = await bcrypt.hash(req.body.password, salt);
-        }
-
-        try {
-            const updatedUser = await User.findByIdAndUpdate(req.params.id, {
-                $set: req.body,
-            },{
-                new: true, 
-            });
-            res.status(200).json(updatedUser)
-
-        } catch (err) {
-            res.status(500).json(err);
-        }
-
-    }   else{
-            res.status(401).json("You can only updaet your account.");
-        }
+  if (req.body.userId === req.params.id) {
+    if (req.body.password) {
+      const salt = await bcrypt.genSalt();
+      req.body.passowrd = await bcrypt.hash(req.body.password, salt);
     }
+
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        {
+          new: true,
+        }
+      );
+      res.status(200).json(updatedUser);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(401).json("You can only updaet your account.");
+  }
 });
+
+module.exports = router;
