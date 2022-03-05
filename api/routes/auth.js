31f09c1e812dfb14c4717a4d737433c7bd8f818e
samir <s-malik@live.co.uk>
2022-03-05 15:33:14 +0000

    Testing API in progress

---
+++ api/routes/auth.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const User = require("../models/User");
+
+//REGISTER
+router.post("/register", async (req, res) => {
+    try {
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+        });
+
+        const user = await newUser.save();
+        console.log("User")
+
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+
+//LOGIN
+
+module.exports = router;
