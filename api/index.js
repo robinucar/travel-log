785a18f77c55c66ee89c5a01132fb1f3a0108c65
samir <s-malik@live.co.uk>
2022-03-05 13:10:16 +0000

    Mongoose connected to backend

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose")
+
+dotenv.config();
+
+mongoose.connect(process.env.API_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(console.log("Connected to MongoDB"))
+.catch((err) => console.log(err));
 
 const PORT = 5001
 
