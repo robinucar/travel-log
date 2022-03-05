ad8a1f5edd518a9846965a09ee303458129eed6f
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-03-05 21:06:47 +0000

    deleted user

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const Post = require("../models/Post")
 const bcrypt = require("bcryptjs");
 
 // UPDATE
@@ -25,7 +26,31 @@ router.put("/:id", async (req, res) => {
       res.status(500).json(err);
     }
   } else {
-    res.status(401).json("You can only updaet your account.");
+    res.status(401).json("You can only UPDATE your account.");
+  }
+});
+
+// DELETE USER
+//delete user by id
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id) {
+    try {
+        const user = await User.findById(req.params.id)
+        try {
+            //delete all user post 
+            await Post.deleteMany({username: user.username})
+            //delete user itself
+            await User.findByIdAndDelete(req.params.id);
+            res.status(200).json("Users Deleted...");
+          }catch (err) {
+            res.status(500).json(err);
+          }
+    }catch(err) {
+        res.status(401).json("User not found...")
+    }
+     
+  } else {
+    res.status(401).json("You can only DELETE your account.");
   }
 });
 
