87129b32ad6d77364e7ed21a045e3c92f65dfa97
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-03-05 23:29:20 +0000

    get post

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -43,13 +43,13 @@ router.put("/:id", async (req, res) => {
 
 // DELETE POST
 router.delete("/:id", async (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
   try {
     const post = await Post.findById(id);
     if (post.username === req.body.username) {
       try {
-        await post.delete()
-        res.status(200).json("Post has been deleted")
+        await post.delete();
+        res.status(200).json("Post has been deleted");
       } catch (err) {
         res.status(500).json(err);
       }
@@ -62,6 +62,14 @@ router.delete("/:id", async (req, res) => {
 });
 
 //GET POST
-router.get("/:id", async (req, res) => {});
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const post = await Post.findById(id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
