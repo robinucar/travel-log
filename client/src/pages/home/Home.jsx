802c41aaa63791111a49f9d8d07748dace4dc762
samir <s-malik@live.co.uk>
2022-03-06 13:55:44 +0000

    Posts Fetch wip

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react";
 import Posts from '../../components/posts/Posts'
 import Sidebar from '../../components/sidebar/Sidebar'
 import Header from '../../components/header/Header';
 import './home.css';
+import axios from 'axios'
 
 export default function Home() {
+
+  const [posts,setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("/posts")
+      console.log(res)
+    }
+    fetchPosts()
+  },[])
+ 
+ 
   return (
     <>
     <Header />
