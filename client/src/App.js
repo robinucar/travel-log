c551eb62e84fda1f653e8b394ad35869f58f7b83
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-03-06 15:48:50 +0000

    get the single post details

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,7 @@ function App() {
           <Route path="/login">{user ? <Home /> : <Login />}</Route>
           <Route path="/write">{user ? <Write /> : <Register />}</Route>
           <Route path="/settings">{user ? <Settings /> : <Register />}</Route>
-          <Route path="/post/:postId">{user ? <Single /> : <Register />}</Route>
+          <Route path="/post/:postId">{user ? <Single /> : <Single />}</Route>
         </Switch>
     </Router>
   );
