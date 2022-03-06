3af50b6b6ec41e93d9b1e7e40d9c777a0ae4a1e3
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-03-06 01:24:05 +0000

    add image

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
+const multer = require('multer');
 const PORT = 5000;
 
 dotenv.config();
@@ -18,6 +19,20 @@ mongoose
   })
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
+
+  const storage = multer.diskStorage({
+    destination:(req, file, callback) => {
+      callback(null, "images")
+    },
+    filename: (req, file, callback) => {
+      callback(null, req.body.name)
+    }
+  })
+
+  const upload = multer({storage})
+  app.post("api/upload", upload.single("file"),(req,res) => {
+    res.status(200).json("File has been uploaded")
+  })
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
