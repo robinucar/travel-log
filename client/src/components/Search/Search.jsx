1eed2f94641cfbf82bc7f3c63bfdde4c003bbb8a
jmodiale <remen.omodiale@outlook.com>
2022-03-08 17:02:33 +0000

    modifications

---
+++ client/src/components/Search/Search.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./search.css";
+
+export default function Search({input:keyword, onChange:setKeyword}) {
+  const styling = {fontFamily: 'EB Garamond', fontSize: '12px'}
+  
+  return (
+    <div className="search">
+      <div className="top-right">
+          <i class="searchIcon fa-solid fa-magnifying-glass"></i>
+          <input
+            style={styling}
+            name="search" 
+            key="random1"
+            value={keyword}
+            placeholder={"SEARCH THE SITE"}
+            onChange={(e) => setKeyword(e.target.value)} 
+        />
+    </div>
+    </div>
+  )
+}
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import Posts from '../../components/posts/Posts'
-import Sidebar from '../../components/sidebar/Sidebar'
 import Header from '../../components/header/Header';
 import './home.css';
+import Search from '../../components/Search/Search'
 import axios from 'axios'
 import { useLocation } from "react-router-dom";
 
@@ -10,24 +10,40 @@ export default function Home() {
 
   const [posts,setPosts] = useState([]);
   const {search} = useLocation()
-
+  const [input, setInput] = useState('');
+  const [postsList, setPostsList] = useState();
   
 
   useEffect(() => {
     const fetchPosts = async () => {
       const res = await axios.get("/posts" + search)
       setPosts(res.data)
+
+      console.log(res.data, 'checking')
+
+      setPostsList(res.data)
     }
     fetchPosts()
   },[search])
- 
+
+
+  const updateInput = async (input) => {
+     const filtered = postsList.filter(posts => {
+      return posts.desc.toLowerCase().includes(input.toLowerCase())
+     })
+     setInput(input);
+     setPosts(filtered);
+  }
  
   return (
     <>
+    <Search
+       input={input} 
+       onChange={updateInput}
+    />
     <Header />
     <div className="home">
         <Posts posts ={posts}/>
-        <Sidebar />
     </div>
     </>
   )
