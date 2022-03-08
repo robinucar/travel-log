89f3713fa0fbbbc1a65bfce280b51a52d96dcabc
Alex Addison <alex.addison@makers.tech>
2022-03-08 15:17:33 +0000

    aa/cc remove debugging stuff from posts.js

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -4,10 +4,6 @@ const Post = require("../models/Post");
 
 //
 // CREATE POST
-//test
-router.post("/posts/upload", async (req, res) => {
-console.log("this is another run...")
-})
 router.post("/", async (req, res) => {
   const newPost = new Post(req.body);
   try {
@@ -16,7 +12,6 @@ router.post("/", async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
-  console.log("I run")
 });
 
 // UPDATE POST
