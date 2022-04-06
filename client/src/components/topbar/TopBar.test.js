7eee96f0f2e3f7667825c7eceae6f67cc0db463f
Cristian Christensen <cc@Cristians-MacBook-Air.local>
2022-04-06 19:40:50 +0200

    Installed testing libraries

---
+++ client/src/components/topbar/TopBar.test.js
@@ -0,0 +1,7 @@
+import React from "react";
+import '@testing-library/jest-dom/extend-expect'
+import { render } from "@testing-library/react";
+import TopBar from "./TopBar";
+
+test("should render", () => {
+})
